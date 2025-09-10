2991e34af8a17ef392a5f0b9af132aad9b696883
Muhammad Mustafa <2mustafa.exe@gmail.com>
2025-09-10 20:45:52 +0500

    🔧 Fix 404 reload issue - Switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
@@ -7,7 +7,7 @@ import { Contact } from './pages/Contact'
 
 export default function App() {
   return (
-    <Router basename="/mustafa-portfolio">
+    <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
