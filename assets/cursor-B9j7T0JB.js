cb93cf92d57616f4929d9e30c8f148648072d1ee
Muhammad Mustafa <2mustafa.exe@gmail.com>
2025-09-10 20:36:44 +0500

    Updates

---
+++ assets/cursor-B9j7T0JB.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-C8OOrmx0.js";import"./index-5Qlm8Y4X.js";import"./events-CO-4yMYj.js";import"./index.es-CgxfsQAI.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
