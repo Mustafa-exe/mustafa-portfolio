f202642d546b285cb38fb009eccf861ef4fdf0b5
Muhammad Mustafa <2mustafa.exe@gmail.com>
2025-09-11 00:12:51 +0500

    Updates

---
+++ assets/all-wallets-Cl08D4gQ.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-BEA1cHj9.js";import"./index-C8pG__08.js";import"./events-Db1LY9G5.js";import"./index.es-DeX7TYoD.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
