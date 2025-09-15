f16175ce1c73cd03b79499cdd5329f082bbd0417
Muhammad Mustafa <2mustafa.exe@gmail.com>
2025-09-15 20:15:24 +0500

    Updates

---
+++ assets/chevron-left-C04Uy5T2.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-CAYTKR9A.js";import"./index-DrIoQG9-.js";import"./events-D_e49LXw.js";import"./index.es-Cy6sc-IK.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as chevronLeftSvg};
---
+++ assets/chevron-right-BMsY-2Qw.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-CAYTKR9A.js";import"./index-DrIoQG9-.js";import"./events-D_e49LXw.js";import"./index.es-Cy6sc-IK.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as chevronRightSvg};
